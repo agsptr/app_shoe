644fa3f84bdaa5f15d3bf4a7a4948d16d7074da5
agsptr <agus.eresha@gmail.com>
2023-11-08 16:54:22 +0700

    update web view

--- lib/detail_menu.dart
+++ lib/detail_menu.dart
@@ -10,6 +10,213 @@ class DetailMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      if (constraints.maxWidth > 800) {
+        return DetailWebPage(data: data);
+      } else {
+        return DetailMobilePage(data: data);
+      }
+    });
+  }
+}
+
+class DetailWebPage extends StatefulWidget {
+  final ModelSepatu data;
+
+  DetailWebPage({Key? key, required this.data}) : super(key: key);
+
+  @override
+  State<DetailWebPage> createState() => _DetailWebPageState();
+}
+
+class _DetailWebPageState extends State<DetailWebPage> {
+  var informationTextStyle = const TextStyle(fontFamily: 'Oxygen');
+
+  final _scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    void dispose() {
+      _scrollController.dispose();
+      super.dispose();
+    }
+
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 64,
+        ),
+        child: Center(
+          child: SizedBox(
+            width: screenWidth <= 1200 ? 800 : 1080,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.discount,
+                      color: Colors.indigo,
+                    ),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Text(
+                      widget.data.merk,
+                      style: TextStyle(
+                        fontFamily: 'Staatliches',
+                        fontSize: 32,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 32),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            child: Image.asset(widget.data.imageAsset),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          SizedBox(height: 16.0),
+                          Scrollbar(
+                            controller: _scrollController,
+                            child: Container(
+                              height: 150,
+                              padding: EdgeInsets.only(bottom: 16),
+                              child: Container(
+                                height: 150,
+                                padding: EdgeInsets.only(bottom: 16.0),
+                                child: ListView(
+                                  controller: _scrollController,
+                                  scrollDirection: Axis.horizontal,
+                                  children: widget.data.imageUrls.map((url) {
+                                    return Padding(
+                                      padding: EdgeInsets.all(4.0),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: Image.network(url),
+                                      ),
+                                    );
+                                  }).toList(),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 32),
+                    Expanded(
+                      child: Card(
+                        child: Container(
+                          padding: EdgeInsets.all(16),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Container(
+                                child: Text(
+                                  widget.data.name,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 30.0,
+                                    fontFamily: 'Staatliches',
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: <Widget>[
+                                      const Icon(
+                                        Icons.people,
+                                        color: Colors.teal,
+                                      ),
+                                      const SizedBox(width: 8.0),
+                                      Text(
+                                        widget.data.gender,
+                                        style: informationTextStyle,
+                                      ),
+                                    ],
+                                  ),
+                                  FavoriteButton(),
+                                ],
+                              ),
+                              SizedBox(height: 8.0),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: <Widget>[
+                                      const Icon(
+                                        Icons.color_lens,
+                                      ),
+                                      const SizedBox(width: 8.0),
+                                      Text(
+                                        widget.data.colorSepatu,
+                                        style: informationTextStyle,
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                        widget.data.priceSepatu,
+                                        style: TextStyle(
+                                          fontSize: 24.0,
+                                          fontFamily: 'Staatliches',
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Container(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8.0),
+                                child: Text(
+                                  widget.data.description,
+                                  textAlign: TextAlign.justify,
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontFamily: 'Oxygen',
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DetailMobilePage extends StatelessWidget {
+  final ModelSepatu data;
+
+  DetailMobilePage({Key? key, required this.data}) : super(key: key);
+
+  var informationTextStyle = const TextStyle(fontFamily: 'Oxygen');
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -127,42 +334,28 @@ class DetailMenu extends StatelessWidget {
               ),
               SizedBox(
                 height: 150,
-                child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: data.imageUrls.map((url) {
-                      return Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.network(
-                            url,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      );
-                    }).toList()),
-              ),
-              SizedBox(height: 8.0),
-              Padding(
-                padding: EdgeInsets.all(8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Beli Langsung'),
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 40, vertical: 8), // Padding tombol
-                      ),
-                    ),
-                    OutlinedButton(
-                      onPressed: () {},
-                      child: Text('Tambahkan Keranjang'),
-                    )
-                  ],
+                child: Scrollbar(
+                  child: Container(
+                    height: 150,
+                    padding: const EdgeInsets.only(bottom: 16),
+                    child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: data.imageUrls.map((url) {
+                          return Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                url,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        }).toList()),
+                  ),
                 ),
               ),
+              SizedBox(height: 8.0),
             ],
           ),
         ),
--- lib/main_menu.dart
+++ lib/main_menu.dart
@@ -8,59 +8,131 @@ class MainMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black54,
-        title: Text('Daftar Sepatu'),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final ModelSepatu data = listModelSepatu[index];
+        appBar: AppBar(
+          backgroundColor: Colors.black54,
+          title: Text('Daftar Sepatu'),
+        ),
+        body: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth <= 600) {
+            // return GridSepatu(gridCount: 2);
+            return ListSepatu();
+          } else if (constraints.maxWidth <= 1200) {
+            return GridSepatu(gridCount: 4);
+          } else {
+            return GridSepatu(gridCount: 6);
+          }
+        }));
+  }
+}
+
+class GridSepatu extends StatelessWidget {
+  final int gridCount;
+  const GridSepatu({Key? key, required this.gridCount}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(24.0),
+      child: GridView.count(
+        crossAxisCount: gridCount,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 16.0,
+        children: listModelSepatu.map((data) {
           return InkWell(
             onTap: () {
               Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return DetailMenu(
-                  data: data,
-                );
+                return DetailMenu(data: data);
               }));
             },
             child: Card(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   Expanded(
-                    flex: 1,
                     child: Image.asset(
                       data.imageAsset,
-                      fit: BoxFit.cover,
+                      fit: BoxFit.contain,
                     ),
                   ),
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: EdgeInsets.all(8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            data.name,
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(data.merk)
-                        ],
-                      ),
+                  SizedBox(height: 8),
+                  Padding(
+                    padding: EdgeInsets.only(left: 8.0),
+                    child: Text(
+                      data.name,
+                      style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w200,
+                          fontFamily: 'Staatliches'),
                     ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 8.0, bottom: 8.0),
+                    child: Text(data.merk),
                   )
                 ],
               ),
             ),
           );
-        },
-        itemCount: listModelSepatu.length,
+        }).toList(),
       ),
+    );
+  }
+}
+
+class ListSepatu extends StatelessWidget {
+  const ListSepatu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final ModelSepatu data = listModelSepatu[index];
+        return InkWell(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return DetailMenu(
+                data: data,
+              );
+            }));
+          },
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Image.asset(
+                    data.imageAsset,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          data.name,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(data.merk)
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+      itemCount: listModelSepatu.length,
     );
   }
 }
